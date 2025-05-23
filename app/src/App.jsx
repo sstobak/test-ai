9c0807c597c60db3666d431d0d2c087ee14c8af3
sstobak <162579461+sstobak@users.noreply.github.com>
2025-05-23 06:14:25 +0000

    feat: add sheep, hippo, goat, and horse SVGs and display them in gallery

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,10 @@
 import chicken from './assets/chicken.svg';
 import cow from './assets/cow.svg';
 import pig from './assets/pig.svg';
+import sheep from './assets/sheep.svg';
+import hippo from './assets/hippo.svg';
+import goat from './assets/goat.svg';
+import horse from './assets/horse.svg';
 import './App.css';
 
 function App() {
@@ -19,6 +23,22 @@ function App() {
         <div className="animal" role="listitem" tabIndex={0} aria-label="Pig">
           <img src={pig} alt="Pig illustration" tabIndex={-1} />
           <div>Pig</div>
+        </div>
+        <div className="animal" role="listitem" tabIndex={0} aria-label="Sheep">
+          <img src={sheep} alt="Sheep illustration" tabIndex={-1} />
+          <div>Sheep</div>
+        </div>
+        <div className="animal" role="listitem" tabIndex={0} aria-label="Hippo">
+          <img src={hippo} alt="Hippo illustration" tabIndex={-1} />
+          <div>Hippo</div>
+        </div>
+        <div className="animal" role="listitem" tabIndex={0} aria-label="Goat">
+          <img src={goat} alt="Goat illustration" tabIndex={-1} />
+          <div>Goat</div>
+        </div>
+        <div className="animal" role="listitem" tabIndex={0} aria-label="Horse">
+          <img src={horse} alt="Horse illustration" tabIndex={-1} />
+          <div>Horse</div>
         </div>
       </div>
     </>
